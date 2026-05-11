9a6184b366b362a2519dff850c644da985ae5560
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-05-11 21:38:24 +0300

    delete and updating  items is done

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,55 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy }) {
+  const handleDelete = () => {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "DELETE"
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log(`Toy with id ${toy.id} deleted successfully.`);
+        } else {
+          console.error(`Failed to delete toy with id ${toy.id}.`);
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting toy:", error);
+      });
+  };
+  const handlelike = () => {
+    const updatedToy = { ...toy, likes: toy.likes + 1 };
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedToy)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`Toy with id ${toy.id} updated successfully:`, data);
+      })
+      .catch((error) => {
+        console.error("Error updating toy:", error);
+      });
+  };
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={handlelike}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={handleDelete}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
