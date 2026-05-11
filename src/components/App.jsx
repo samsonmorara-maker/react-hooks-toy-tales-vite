f3b7fbffbf43b9b52637fe3e39e1ae7af67ff3c9
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-05-11 21:14:33 +0300

    fetching data from server but not rendering it yet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-
+import { useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch("http://localhost:3001/toys")
+        .then((response) => response.json())
+        .then((data) => setToys(data));
+    } catch (error) {
+      console.error("Error fetching toys:", error);
+    }
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
